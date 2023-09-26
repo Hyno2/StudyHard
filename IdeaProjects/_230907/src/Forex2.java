512ea46895148a20877f2dd07fbc8734e4b6d18b
Hyno2 <say01251@gmail.com>
2023-09-26 19:35:49 +0900

    wl

--- IdeaProjects/_230907/src/Forex2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Forex2 {
-    public static void main(String[] args) {
-        // 16. 두 수를 입력 받아 두 수의 최대 공약수,
-        // 최소 공배수를 구하는 프로그램을 작성하시오. 유클리드 호제법을 이용하시오.
-
-    }
-}
